b1ffa32a93cd7c3ada7c6a8bd8fcf52a97cdf62d
Edson L Parisotto <parisotto@gmail.com>
2016-11-16 01:17:10 -0200

    Update Quiz.java
    Inclusão das questões.

--- tp5quiz/src/java/quiz/Quiz.java
+++ tp5quiz/src/java/quiz/Quiz.java
@@ -25,60 +25,135 @@ public class Quiz{
         if(Quiz.test == null){
             Quiz.test = new ArrayList<>();
             
-            // Questão 1 resposta índice 2
+            // Questão 1 resposta índice 3
             Question q1 = new Question(
-                    "Qual a definição de Classe em POO?", 2, 
+                    "Como teve início a Ditadura Militar no Brasil que durou de 1964 a 1985?", 
+                    3, 
                     new String[]{
-                        "É um objeto após ser instanciado", 
-                        "É o protótipo de um vetor com atributos", 
-                        "É uma abstração que define um tipo de objeto" 
+                        "A - Através de eleições democráticas que levaram ao poder os militares.", 
+                        "B - Através de um acordo ocorrido entre o presidente João Goulart e comandantes das forças armadas brasileiras.", 
+                        "C - Através de uma sangrenta guerra civil em que os militares tomaram o poder a força, após a morte de milhares de brasileiros.",
+                        "D - Através de um golpe militar, ocorrido em 31 de março de 1964, que tirou o presidente João Goulart do poder."
                     }
             );
             Quiz.test.add(q1);
             
             // Questão 2 resposta índice 0
             Question q2 = new Question(
-                    "O que é um atributo?", 0, 
+                    "Qual das alternativas abaixo aponta características do regime militar brasileiro?", 
+                    0, 
                     new String[]{
-                        "Esta é a resposta correta", 
-                        "É o protótipo de um vetor com atributos", 
-                        "É uma abstração que define um tipo de objeto" 
+                        "A - Bipartidarismo, falta de democracia, perseguição aos opositores políticos e repressão aos movimentos sociais.", 
+                        "B - Democracia, eleições diretas para presidência da República, apoio aos movimentos sociais, distribuição de terras para os camponeses.", 
+                        "C - Implantação do socialismo, existência de vários partidos políticos, tolerância com os opositores políticos.",
+                        "D - Liberdade de imprensa, valorização do sistema democrático, apoio aos sindicatos e movimentos de trabalhadores sem terras."
                     }
             );
             Quiz.test.add(q2);
             
-            // Questão 3 resposta índice 2
+            // Questão 3 resposta índice 3
             Question q3 = new Question(
-                    "O que é um atributo?", 2, 
+                    "O Ato Institucional Número 5 ( AI-5 ) foi o mais duro do governo militar. Qual das alternativas abaixo aponta as características deste ato?", 
+                    3, 
                     new String[]{
-                        "É o protótipo de um vetor com atributos", 
-                        "É uma abstração que define um tipo de objeto",
-                        "Esta é a resposta correta" 
+                        "A - Fechou totalmente a economia brasileira para o mercado externo, nomeou juízes para cargos públicos e implantou várias medidas socialistas.", 
+                        "B - Decretou a prisão de todos os artistas brasileiros, proibiu todo tipo de música no país e modificou a letra do Hino Nacional.",
+                        "C - Decretou o fechamento do Congresso Nacional por 10 anos, anulou o título de eleitor de todos os cidadãos brasileiros e anulou as aposentadorias de cantores, professores e políticos.",
+                        "D - Aposentou juízes, cassou mandatos, acabou com as garantias do habeas-corpus e aumentou a repressão militar e policial."
                     }
             );
             Quiz.test.add(q3);
             
             // Questão 4 resposta índice 1
             Question q4 = new Question(
-                    "O que é um atributo?", 1, 
+                    "No campo econômico, a economia brasileira cresceu muito durante os anos de 1969 e 1973. Esta fase ficou conhecida como época do Milagre Econômico. Qual das alternativas abaixo aponta características deste período?", 
+                    1, 
                     new String[]{
-                        "É o protótipo de um vetor com atributos", 
-                        "Esta é a resposta correta", 
-                        "É uma abstração que define um tipo de objeto" 
+                        "A - Investimentos nos setores culturais e educacionais, baixo índice de endividamento externo, distribuição de renda de forma justa.", 
+                        "B - Forte crescimento do PIB, investimentos em infraestrutura e elevados empréstimos vindos do exterior com aumento da dívida externa.", 
+                        "C - Elevados investimentos externos (principalmente da URSS), inflação muito baixa e controlada e aumento do consumo das camadas mais pobres da sociedade.",
+                        "D - Criação de programas de distribuição de renda, incentivo à reforma agrária, aumento significativo das exportações de máquinas e produtos tecnológicos."
                     }
             );
             Quiz.test.add(q4);
             
-            // Questão 5 resposta índice 0
+            // Questão 5 resposta índice 2
             Question q5 = new Question(
-                    "O que é um atributo?", 0, 
+                    "Qual das alternativas abaixo aponta uma importante decisão que abriu caminho para a redemocratização no país e fim da ditadura militar?", 
+                    2, 
                     new String[]{
-                        "Esta é a resposta correta", 
-                        "É o protótipo de um vetor com atributos", 
-                        "É uma abstração que define um tipo de objeto" 
+                        "A - Em 1984, a Campanha das Diretas Já conquistou seu objetivo e as eleições diretas para presidente voltou ao país em 1985.", 
+                        "B - Em 1979, o presidente Figueiredo estabeleceu o fim da ditadura e convocou eleições diretas para presidente.", 
+                        "C - Em 1978, o presidente Geisel acabou com o AI-5, restaurou o habeas-corpus e abriu caminho para a volta da democracia no Brasil.",
+                        "D - Em 1980, com apoio dos EUA, uma guerra civil derrubou o governo militar e colocou no poder um presidente civil."
                     }
             );
             Quiz.test.add(q5);
+            
+            //Questão 6 resposta índice 2
+            Question q6 = new Question(
+                    "Qual alternativa relaciona três militares que assumiram a presidência do Brasil durante o golpe militar?",
+                    2,
+                    new String[]{
+                        "A - Deodoro da Fonseca, Floriano Peixoto, Ernesto Geisel",
+                        "B - Emílio Garrastazu Médici, Getúlio Vargas, Gaspar Dutra",
+                        "C - Erneto Geisel, João Figueiredo, Emílio Garrastazu Médici",
+                        "D - Getúlio Vargas, Tancredo Neves, Catulo da Paixão Cearense"
+                    }
+            );
+            Quiz.test.add(q6);
+            
+            //Questão 7 resposta índice 3
+            Question q7 = new Question(
+                    "Quem era o presidente civil deposto pelo golpe militar?",
+                    3,
+                    new String[]{
+                        "A - Tancredo Neves",
+                        "B - Luis Inácio Lula da Silva",
+                        "C - Fernando Collor de Melo",
+                        "D - João Goulart"
+                    }
+            );
+            Quiz.test.add(q7);
+            
+            //Questão 8 resposta índice 1
+            Question q8 = new Question(
+                    "Quem foi o presidente eleito por voto INDIRETO após o fim do regime militar?",
+                    1,
+                    new String[]{
+                        "A - Fernando Henrique Cardoso",
+                        "B - Tancredo Neves",
+                        "C - José Sarney",
+                        "D - Lula"
+                    }
+            );
+            Quiz.test.add(q8);
+            
+            //Questão 9 resposta índice 2
+            Question q9 = new Question(
+                    "Quem foi o primeiro presidente eleito por voto DIRETO do povo após o fim do regime militar?",
+                    2,
+                    new String[]{
+                        "A - Fernando Henrique Cardoso",
+                        "B - Luis Inácio Lula da Silva",
+                        "C - Fernando Collor de Melo",
+                        "D - Itamar Franco"
+                    }
+            );
+            Quiz.test.add(q9);
+            
+            //Questão 10 resposta índice 0
+            Question q10 = new Question(
+                    "Quem foi o primeiro presidente militar a assumir quando ocorreu o golpe de 1964?",
+                    0,
+                    new String[]{
+                        "A - Castelo Branco",
+                        "B - Costa e Silva",
+                        "C - Emílio Garrastazu Médici",
+                        "D - Ernesto Geisel"
+                    }
+            );
+            Quiz.test.add(q10);
         }
         return Quiz.test;
     }
